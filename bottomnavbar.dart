6c73849b0d4f6820875c23ff265c3c5798f5a845
Mithila <150042569+NKDMMadhusankha@users.noreply.github.com>
2024-04-12 01:42:04 +0530

    Mithila

---
+++ bottomnavbar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BottomNavbar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const BottomNavbar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedItemColor: Colors.black,
+      unselectedItemColor: Colors.black.withOpacity(0.6),
+      backgroundColor: Colors.white,
+      elevation: 10,
+      selectedFontSize: 14,
+      unselectedFontSize: 12,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add),
+          label: 'Add',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'Favorite',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          label: 'Account',
+        ),
+      ],
+    );
+  }
+}
